fc4548b6b6ad0eaf14e60bba7e013a88e0f22868
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-10-11 17:12:14 +0800

    [leetcode] 165. 比较版本号

---
+++ src/leet-code/compare-version.ts
@@ -0,0 +1,32 @@
+/**
+ * [165. 比较版本号](https://leetcode.cn/problems/compare-version-numbers)
+ */
+function compareVersion(version1: string, version2: string): number {
+  let i = 0;
+  let j = 0;
+  while (i < version1.length || j < version2.length) {
+    let reversion1 = 0;
+    while (i < version1.length && version1[i] !== ".") {
+      reversion1 =
+        reversion1 * 10 + version1[i].charCodeAt(0) - "0".charCodeAt(0);
+      i++;
+    }
+    i++;
+
+    let reversion2 = 0;
+    while (j < version2.length && version2[j] !== ".") {
+      reversion2 =
+        reversion2 * 10 + version2[j].charCodeAt(0) - "0".charCodeAt(0);
+      j++;
+    }
+    j++;
+
+    if (reversion1 > reversion2) {
+      return 1;
+    } else if (reversion1 < reversion2) {
+      return -1;
+    }
+  }
+
+  return 0;
+}
