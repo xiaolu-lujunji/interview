c8e836f022cd0b6820f46ee7ff96a6e575bfaa7b
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-10-13 15:27:45 +0800

    [leetcode] 129. 求根节点到叶节点数字之和

---
+++ src/leet-code/leet-code.d.ts
@@ -0,0 +1,6 @@
+//Definition for a binary tree node.
+declare class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+}
---
+++ src/leet-code/sum-numbers.ts
@@ -0,0 +1,20 @@
+/**
+ * @source [129. 求根节点到叶节点数字之和](https://leetcode.cn/problems/sum-root-to-leaf-numbers/)
+ * @tag 深度优先搜索
+ */
+function sumNumbers(root: TreeNode | null): number {
+  if (root === null) return 0;
+  const dfs = (root: TreeNode, acc: number) => {
+    const newAcc = acc * 10 + root.val;
+    if (root.left === null && root.right === null) return newAcc;
+    let ret = 0;
+    if (root.left) {
+      ret += dfs(root.left, newAcc);
+    }
+    if (root.right) {
+      ret += dfs(root.right, newAcc);
+    }
+    return ret;
+  };
+  return dfs(root, 0);
+}
