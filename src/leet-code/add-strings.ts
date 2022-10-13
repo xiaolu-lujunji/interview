2860b9b077cee394fd072bf22f59d7b7e304dac6
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-10-13 14:56:50 +0800

    [leetcode] 415. 字符串相加

---
+++ src/leet-code/add-strings.ts
@@ -0,0 +1,24 @@
+/**
+ * @source [415. 字符串相加](https://leetcode.cn/problems/add-strings/)
+ * @tag 模拟
+ */
+function addStrings(num1: string, num2: string): string {
+  let result = "";
+  let carry = 0;
+  let i = num1.length - 1;
+  let j = num2.length - 1;
+  while (i >= 0 || j >= 0 || carry === 1) {
+    const number1 = i >= 0 ? num1.charCodeAt(i) - "0".charCodeAt(0) : 0;
+    const number2 = j >= 0 ? num2.charCodeAt(j) - "0".charCodeAt(0) : 0;
+    let sum = number1 + number2 + carry;
+    carry = 0;
+    if (sum >= 10) {
+      carry = 1;
+      sum = sum % 10;
+    }
+    result = sum + result;
+    i--;
+    j--;
+  }
+  return result;
+}
