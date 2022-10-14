3c542de551fbe9ed267e7c513cd4c6d48c739a5f
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-10-14 10:09:33 +0800

    [leetcode] 15. 三数之和

---
+++ src/leet-code/three-sum.ts
@@ -0,0 +1,30 @@
+/**
+ * @source [15. 三数之和](https://leetcode.cn/problems/3sum/)
+ * @tag 排序 双指针
+ */
+function threeSum(nums: number[]): number[][] {
+  const sortedNums = nums.sort((a, b) => a - b);
+  const result: number[][] = [];
+  for (let k = 0; k < sortedNums.length; ++k) {
+    if (k > 0 && sortedNums[k] === sortedNums[k - 1]) continue;
+    let i = k + 1;
+    let j = sortedNums.length - 1;
+    while (i < j) {
+      if (i - 1 > k && sortedNums[i] === sortedNums[i - 1]) {
+        i++;
+        continue;
+      }
+      const sum = sortedNums[k] + sortedNums[i] + sortedNums[j];
+      if (sum === 0) {
+        result.push([sortedNums[k], sortedNums[i], sortedNums[j]]);
+        i++;
+        j--;
+      } else if (sum < 0) {
+        i++;
+      } else {
+        j--;
+      }
+    }
+  }
+  return result;
+}
