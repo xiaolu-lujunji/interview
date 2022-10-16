21ed0653d3bebf03bcf2b0991eac0e3820df2353
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-10-16 11:08:24 +0800

    [leetcode] 200. 岛屿数量

---
+++ src/leet-code/num-islands.ts
@@ -0,0 +1,37 @@
+/**
+ * @source [200. 岛屿数量](https://leetcode.cn/problems/number-of-islands/)
+ * @tag 深度优先搜索 广度优先搜索 并查集
+ */
+function numIslands(grid: string[][]): number {
+  const rows = grid.length;
+  const columns = grid[0].length;
+
+  const walk = (i: number, j: number) => {
+    if (grid[i][j] === "0") return;
+
+    grid[i][j] = "0";
+
+    if (i - 1 >= 0) {
+      walk(i - 1, j);
+    }
+    if (j + 1 < columns) {
+      walk(i, j + 1);
+    }
+    if (i + 1 < rows) {
+      walk(i + 1, j);
+    }
+    if (j - 1 >= 0) {
+      walk(i, j - 1);
+    }
+  };
+
+  let result = 0;
+  for (let i = 0; i < rows; ++i) {
+    for (let j = 0; j < columns; ++j) {
+      if (grid[i][j] === "1") result++;
+      walk(i, j);
+    }
+  }
+
+  return result;
+}
