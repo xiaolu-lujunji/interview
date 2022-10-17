072433d86e636ce5412317a2deb700a9f6358304
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-10-17 22:45:37 +0800

    [leetcode ] 70. 爬楼梯

---
+++ src/leet-code/climb-stairs.ts
@@ -0,0 +1,13 @@
+/**
+ * @source [70. 爬楼梯](https://leetcode.cn/problems/climbing-stairs/)
+ * @tag 动态规划 矩阵快速幂 通项公式
+ */
+function climbStairs(n: number): number {
+  const state: number[] = [1];
+
+  for (let i = 1; i <= n; ++i) {
+    state[i] = state[i - 1] + (i - 2 < 0 ? 0 : state[i - 2]);
+  }
+
+  return state[n];
+}
