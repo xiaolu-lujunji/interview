e74ea761c6804bca5d0c17cf5ccab0c015463f10
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-10-17 21:44:58 +0800

    [leetcode] 209. 长度最小的子数组

---
+++ src/leet-code/min-sub-array-len.ts
@@ -0,0 +1,22 @@
+/**
+ * @source [209. 长度最小的子数组](https://leetcode.cn/problems/minimum-size-subarray-sum/)
+ * @tag 滑动窗口
+ */
+function minSubArrayLen(target: number, nums: number[]): number {
+  let i = 0;
+  let j = 0;
+  let result = nums.length + 1;
+  let sum = 0;
+  while (j < nums.length) {
+    while (j < nums.length && sum < target) {
+      sum += nums[j];
+      j++;
+    }
+    while (i < nums.length && sum >= target) {
+      result = Math.min(result, j - i);
+      sum -= nums[i];
+      i++;
+    }
+  }
+  return result === nums.length + 1 ? 0 : result;
+}
