388bbbe89a1c74edd878295651238833078ddd48
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-10-18 08:45:53 +0800

    [leetcode] 剑指 Offer 22. 链表中倒数第k个节点

---
+++ src/leet-code/get-kth-from-end.ts
@@ -0,0 +1,23 @@
+/**
+ * @source [剑指 Offer 22. 链表中倒数第k个节点](https://leetcode.cn/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/)
+ * @tag 两次遍历 快慢指针
+ */
+function getKthFromEnd(head: ListNode | null, k: number): ListNode | null {
+  let len = 0;
+
+  let p = head;
+  while (p) {
+    len++;
+    p = p.next;
+  }
+
+  let targetIndex = len - k;
+  let index = 0;
+  while (head) {
+    if (index === targetIndex) return head;
+    index++;
+    head = head.next;
+  }
+
+  return null;
+}
