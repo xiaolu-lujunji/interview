236cf78bb21f64a6125a626c26d1ae89c8de6916
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-10-20 09:54:31 +0800

    [leetcode] 103. 二叉树的锯齿形层序遍历

---
+++ src/leet-code/zigzag-level-order.ts
@@ -0,0 +1,27 @@
+/**
+ * @source [103. 二叉树的锯齿形层序遍历](https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal/submissions/)
+ * @tag 广度优先搜索
+ */
+function zigzagLevelOrder(root: LeetCode.TreeNode | null): number[][] {
+  const queue: LeetCode.TreeNode[] = [];
+  if (root) queue.push(root);
+
+  const result: number[][] = [];
+
+  let toRight = true;
+
+  while (queue.length > 0) {
+    let len = queue.length;
+    const values: number[] = [];
+    for (let i = 0; i < len; ++i) {
+      const node = queue.shift()!;
+      toRight ? values.push(node.val) : values.unshift(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(values);
+    toRight = !toRight;
+  }
+
+  return result;
+}
