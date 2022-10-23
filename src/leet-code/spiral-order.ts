3ea63ae1c48ff24678dedae522bf6487848cce3b
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-10-23 09:26:10 +0800

    [leetcode] 剑指 Offer 29. 顺时针打印矩阵

--- src/leet-code/spiral-order.ts
+++ src/leet-code/spiral-order.ts
@@ -40,3 +40,39 @@ function spiralOrder(matrix: number[][]): number[] {
     }
   }
 }
+
+/**
+ * @description 剑指 Offer 29. 顺时针打印矩阵
+ * @see https://leetcode.cn/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/
+ * @tag 模拟
+ */
+function spiralOrder2(matrix: number[][]): number[] {
+  const row = matrix.length;
+  const column = matrix[0] ? matrix[0].length : 0;
+  const size = row * column;
+
+  const result: number[] = [];
+
+  let i = 0;
+  let j = -1;
+  while (result.length < size) {
+    while (j + 1 < column && !Number.isNaN(matrix[i][j + 1])) {
+      result.push(matrix[i][++j]);
+      matrix[i][j] = NaN;
+    }
+    while (i + 1 < row && !Number.isNaN(matrix[i + 1][j])) {
+      result.push(matrix[++i][j]);
+      matrix[i][j] = NaN;
+    }
+    while (j - 1 > -1 && !Number.isNaN(matrix[i][j - 1])) {
+      result.push(matrix[i][--j]);
+      matrix[i][j] = NaN;
+    }
+    while (i - 1 > -1 && !Number.isNaN(matrix[i - 1][j])) {
+      result.push(matrix[--i][j]);
+      matrix[i][j] = NaN;
+    }
+  }
+
+  return result;
+}
